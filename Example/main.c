843cd1c4736ed5a8b9615f5ddaa83fe92e449753
SubhransuPrabhat <subhransu242@gmail.com>
2026-08-31 17:51:06 +0530

    Add new CLI interface in Example/

--- Example/main.c
+++ Example/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <calculator.h>
+#include <stdlib.h>
+#include "calculator.h"
+#include "error.h"
 
-int main(int argc,char *argv[]){
-    if(argc!=2){
-        printf("Usage: %s <expression>",argv[0]);
-        return 1;
-    }
+int main(){
+    char exp[20];
     
-    Result result = calculate(argv[1]);
-
-    if(result.Error != NO_ERROR){
-        print_error(result.Error);
-        return 1;
+    printf("C-Calculator\n");
+    while(1){
+        printf(">>>");
+        fgets(exp,sizeof(exp),stdin);
+        Result result = calculate(exp);
+        if(result.Error != NO_ERROR){
+            print_error(result.Error);
+        }else{
+            printf("%f\n",result.value);            
+        }
     }
-    
-    printf("%f",result.value);
 }
--- src/error.c
+++ src/error.c
@@ -4,26 +4,26 @@
 void print_error(Errortype error){
     switch(error){
     case INVALID_CHARACTER :
-        fprintf(stderr,"Invalid character.\n");
+        fprintf(stderr,"ERROR: Invalid character.\n");
         break;
     case INVALID_EXPRESSION :
-        fprintf(stderr,"Invalid expression.\n");
+        fprintf(stderr,"ERROR: Invalid expression.\n");
         break;
     case MISMATCHED_PARAN :
-        fprintf(stderr,"Mismatched parentheses.\n");
+        fprintf(stderr,"ERROR: Mismatched parentheses.\n");
         break;
     case MISSING_OPERAND :
-        fprintf(stderr,"Missing operand.\n");
+        fprintf(stderr,"ERROR: Missing operand.\n");
         break;
     case MISSING_OPERATOR :
-        fprintf(stderr,"Missing operator.\n");
+        fprintf(stderr,"ERROR: Missing operator.\n");
         break;
     case DIVISION_BY_ZERO :
-        fprintf(stderr,"Divison by zero.\n");
+        fprintf(stderr,"ERROR: Divison by zero.\n");
         break;
     case NO_ERROR : break;    
     default :
-        fprintf(stderr,"Unknown error.\n");
+        fprintf(stderr,"ERROR: Unknown error.\n");
         break;
     }
 }
